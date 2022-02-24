fbbe2fcfbdabd7e1361322dd16cc7cbb0a4a4a4a
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2022-02-24 17:16:55 -0500

    graphql codegen overhaul to watch on workspace open
    this fixes an issue with an annoying empty folder being created for no reason, and allows to automatically watch graphql files in the background
    
    this removes some scripts (those which gave a "no watch" option which unclutters the tasks, launch options and package scripts
    
    there is now a small issue where if you close the client debugger before the api debugger, you will be asked to terminate a task even if you didn't need to, but that's not that big of an issue compared to the improvements this does
    
    no need to wonder if the graphql codegen is active or not anymore, nor do you need to be actively debugging to generate graphql typescript types!

--- .graphqlrc.js
+++ .graphqlrc.js
@@ -1,48 +1,36 @@
 const dotenv = require('dotenv');
 
 const apiEnv = dotenv.config({ path: `${__dirname}/api/.env` }).parsed ?? {};
-const clientEnv = dotenv.config({ path: `${__dirname}/client/.env` }).parsed ?? {};
-
-/** @type {import('@graphql-codegen/typescript-operations').TypeScriptDocumentsPluginConfig} */
-const tsOperationConfig = {
-	useTypeImports: true,
-};
-
-/** @type {import('@graphql-codegen/typed-document-node').TypeScriptTypedDocumentNodesConfig} */
-const typedDocNodeConfig = {
-	useTypeImports: true,
-};
-
-/** @type {import('@graphql-codegen/plugin-helpers').Types.Config} */
-const clientCodegenConfig = {
-	generates: {
-		'client/src/graphql/@generated/index.ts': {
-			plugins: [
-				{
-					typescript: {},
-				},
-				{
-					'typescript-operations': tsOperationConfig,
-				},
-				{
-					'typed-document-node': typedDocNodeConfig,
-				},
-			],
-		},
-	},
-};
+// const clientEnv = dotenv.config({ path: `${__dirname}/client/.env` }).parsed ?? {};
 
 /** @type {import('graphql-config').IGraphQLConfig} */
 const config = {
-	projects: {
-		api: {
-			schema: `http://localhost:${apiEnv.PORT ?? 3000}/graphql`,
-		},
-		client: {
-			schema: `${clientEnv.VITE_API_ADDR}/graphql`,
-			documents: 'client/src/graphql/**/*.graphql',
-			extensions: {
-				codegen: clientCodegenConfig,
+	schema: `http://localhost:${apiEnv.PORT ?? 3000}/graphql`,
+	documents: 'client/src/graphql/**/*.graphql',
+	extensions: {
+		/** @type {import('@graphql-codegen/plugin-helpers').Types.Config} */
+		codegen: {
+			generates: {
+				'client/src/graphql/@generated/index.ts': {
+					plugins: [
+						{
+							/** @type {import('@graphql-codegen/typescript').TypeScriptPluginConfig} */
+							typescript: {},
+						},
+						{
+							/** @type {import('@graphql-codegen/typescript-operations').TypeScriptDocumentsPluginConfig} */
+							'typescript-operations': {
+								useTypeImports: true,
+							},
+						},
+						{
+							/** @type {import('@graphql-codegen/typed-document-node').TypeScriptTypedDocumentNodesConfig} */
+							'typed-document-node': {
+								useTypeImports: true,
+							},
+						},
+					],
+				},
 			},
 		},
 	},
