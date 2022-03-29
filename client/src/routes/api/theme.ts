934f7499f943b1e2881eda614f83ba246ea10e47
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2022-03-29 12:35:27 -0400

    chore: fix theme preventing direct get access
    sure, it worked if you navigated using `goto` in the code, but reloading the page or sharing the link would cause issues

--- client/src/routes/api/theme.ts
+++ client/src/routes/api/theme.ts
@@ -3,7 +3,7 @@ import type { RequestHandler } from '@sveltejs/kit';
 export const put: RequestHandler = async () => {
 	return {
 		body: {
-			data: 'Hi there!'
-		}
+			data: 'Hi there!',
+		},
 	};
 };
