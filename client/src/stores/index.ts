e2038b5aae6f0dd80f0ccf38db8ad05ae2994c9e
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2022-04-02 15:02:32 -0400

    chore: remove unused comment

--- client/src/stores/index.ts
+++ client/src/stores/index.ts
@@ -1,17 +1,7 @@
 import { writable } from 'svelte/store';
-// import { makeRefreshable } from './refreshable';
 import { makeToggleable } from './toggleable';
 
-// let breakpoints: typeof BreakpointVariants | undefined = undefined;
-
-// export function setBreakpoints(newBreakpoints: typeof BreakpointVariants): void {
-// 	breakpoints = newBreakpoints;
-// }
-
 export const isDrawerOpen = makeToggleable(writable(false), (drawerOpenStore) => drawerOpenStore.update((isOpen) => !isOpen));
-// export const isMobile = makeRefreshable(writable(true), (isMobileStore) => {
-// 	return breakpoints ? isMobileStore.set(window.matchMedia(breakpoints['md-and-down']).matches) : true;
-// });
 
 export * from './local-storage';
 export * from './matchMedia';
