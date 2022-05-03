f4509b3b85ec2240b029cf1b11267815508a423d
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2022-05-03 16:58:24 -0400

    chore: make graphql module async and inject config module
    this would make dep injection easier if I was ever to test it

--- api/src/@common/graphql.module.ts
+++ api/src/@common/graphql.module.ts
@@ -1,19 +1,25 @@
-import { env } from '$common/configs';
-import { Environment } from '$common/configs/env.validation';
+import { Environment, EnvironmentConfig } from '$common/configs/env.validation';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GraphQLModule as NestGraphQLModule } from '@nestjs/graphql';
 import depthLimit from 'graphql-depth-limit';
+import { ConfigModule } from './configs/config.module';
 
-const isProd = env.NODE_ENV == Environment.Production;
-
-export const GraphQLModule = NestGraphQLModule.forRoot<ApolloDriverConfig>({
+export const GraphQLModule = NestGraphQLModule.forRootAsync<ApolloDriverConfig>({
 	driver: ApolloDriver,
-	autoSchemaFile: true,
-	installSubscriptionHandlers: true,
-	subscriptions: {
-		'graphql-ws': true,
+	imports: [ConfigModule],
+	useFactory: (env: EnvironmentConfig) => {
+		const isProd = env.NODE_ENV == Environment.Production;
+
+		return {
+			autoSchemaFile: true,
+			installSubscriptionHandlers: true,
+			subscriptions: {
+				'graphql-ws': true,
+			},
+			debug: !isProd,
+			playground: !isProd,
+			validationRules: [depthLimit(env.GRAPHQL_DEPTH_LIMIT)],
+		};
 	},
-	debug: !isProd,
-	playground: !isProd,
-	validationRules: [depthLimit(env.GRAPHQL_DEPTH_LIMIT)],
+	inject: [EnvironmentConfig],
 });
