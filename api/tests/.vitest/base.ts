05640fffed1fc9e9b13447532e29bf606e843072
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2022-05-09 14:34:42 -0400

    chore(tests): allow env variables to change reporter and directory
    this also fixes the PR coverage action, using vitest with the action's expectations

--- api/tests/.vitest/base.ts
+++ api/tests/.vitest/base.ts
@@ -1,18 +1,24 @@
 import deepmerge from 'deepmerge';
 import VitePluginTsConfigPaths from 'vite-tsconfig-paths';
-import { defineConfig } from 'vitest/config';
+import { defineConfig, UserConfig } from 'vitest/config';
 import { env } from '../../src/@common/configs';
 
 const dbURL = process.env.TEST_DATABASE_URL ?? env.TEST_DATABASE_URL;
-
 process.env.DATABASE_URL = dbURL;
+
+type CoverageReporter = UserConfig['test']['coverage']['reporter'];
+
+const coverageReporter: CoverageReporter = (process.env.COV_REPORTER as CoverageReporter) ?? 'lcov';
+const coverageDirectory: string = process.env.COV_DIRECTORY ?? `./coverage`;
 
 const vitestBaseConfig = defineConfig({
 	plugins: [VitePluginTsConfigPaths({ loose: true })],
 	test: {
 		testTimeout: 30000,
 		coverage: {
-			reportsDirectory: `./coverage`,
+			reportsDirectory: coverageDirectory,
+			reporter: coverageReporter,
+			clean: false,
 			include: ['**/*.ts'],
 			exclude: ['**/*.d.ts', '.graphqlrc.ts', 'gulpfile.ts', '**/_generated/**', '**/prisma/**', '**/dist/**', '**/fixtures/**'],
 		},
