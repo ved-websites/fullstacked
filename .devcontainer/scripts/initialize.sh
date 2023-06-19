d80fac5875ec1e2f99995e0cff32b6a504f98785
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2023-06-19 18:57:18 -0400

    chore: make sure initialize script uses proper notation for pgrep
    if multiple would be found (very, very unlikely), properly format the list to use with kill command

--- .devcontainer/scripts/initialize.sh
+++ .devcontainer/scripts/initialize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-API_DEV_PID=$(pgrep -f 'pnpm start:debug')
+API_DEV_PID=$(pgrep -f 'pnpm start:debug' -d ' ')
 
 # If there is a api dev server already running, kill it.
 if [ ! -z $API_DEV_PID ]
@@ -8,7 +8,7 @@ then
 	kill $API_DEV_PID
 fi
 
-CLIENT_DEV_PID=$(pgrep -f 'vite\.js dev')
+CLIENT_DEV_PID=$(pgrep -f 'vite\.js dev' -d ' ')
 
 # If there is a vite dev server already running, kill it.
 if [ ! -z $CLIENT_DEV_PID ]
