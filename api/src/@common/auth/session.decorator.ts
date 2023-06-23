4e13ccdf7a570224ec085dfbce6442f1697e0fcd
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2023-06-23 18:36:26 -0400

    chore: fix api wrong context access for AuthSession

--- api/src/@common/auth/session.decorator.ts
+++ api/src/@common/auth/session.decorator.ts
@@ -1,8 +1,8 @@
+import { getGraphQLRequest } from '$common/utils/contextExtracter';
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
-import type { Request } from 'express';
 
 export const AuthSession = createParamDecorator(async (_data, context: ExecutionContext) => {
-	const { session } = context.switchToHttp().getRequest() as Request;
+	const { session } = getGraphQLRequest(context);
 
 	return session;
 });
