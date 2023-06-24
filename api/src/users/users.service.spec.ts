389c76facda2089309d8c2406003b80aec5f6b2e
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2023-06-24 05:28:39 -0400

    chore: fix tests

--- api/src/users/users.service.spec.ts
+++ api/src/users/users.service.spec.ts
@@ -1,18 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { beforeEach, describe, expect, it } from 'vitest';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
-  let service: UsersService;
+	let service: UsersService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [UsersService],
+		}).compile();
 
-    service = module.get<UsersService>(UsersService);
-  });
+		service = module.get<UsersService>(UsersService);
+	});
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
 });
