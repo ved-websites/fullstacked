c3fd3e51d12481e5bdc03f8d2007bf591897a4f3
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2023-07-14 23:15:07 -0400

    chore: move users module to common
    having users will be a common trope, and therefore having it in the commons folder is more than appropriate

--- api/src/@common/app/app.helpers.ts
+++ api/src/@common/app/app.helpers.ts
@@ -3,6 +3,14 @@ import { ConfigModule } from '$configs/config.module';
 import { EmailModule } from '$email/email.module';
 import { GraphQLModule } from '$graphql/graphql.module';
 import { PrismaModule } from '$prisma/prisma.module';
+import { UsersModule } from '$users/users.module';
 import { ModuleMetadata } from '@nestjs/common';
 
-export const BaseModules = [ConfigModule, EmailModule, PrismaModule, GraphQLModule, AuthModule] satisfies ModuleMetadata['imports'];
+export const BaseModules = [
+	ConfigModule,
+	EmailModule,
+	PrismaModule,
+	GraphQLModule,
+	AuthModule,
+	UsersModule,
+] satisfies ModuleMetadata['imports'];
--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,9 +4,8 @@ import { Module, type ModuleMetadata } from '@nestjs/common';
 import { AppController } from './@common/app/app.controller';
 import { BaseModules } from './@common/app/app.helpers';
 import { MessageModule } from './message/message.module';
-import { UsersModule } from './users/users.module';
 
-const AppImports = [MessageModule, UsersModule] satisfies ModuleMetadata['imports'];
+const AppImports = [MessageModule] satisfies ModuleMetadata['imports'];
 
 @Module({
 	imports: [...BaseModules, OnboardingModule, ...AppImports],
