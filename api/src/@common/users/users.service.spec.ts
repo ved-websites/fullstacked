7ff1f9968347b4203b4b38190d24150413fc33d3
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2023-07-18 23:55:00 -0400

    chore: remove problematic LuciaFactory empty provider

--- api/src/@common/users/users.service.spec.ts
+++ api/src/@common/users/users.service.spec.ts
@@ -1,5 +1,4 @@
 import { AuthModule } from '$auth/auth.module';
-import { LuciaFactory } from '$auth/lucia/lucia.factory';
 import { RolesService } from '$auth/roles/roles.service';
 import { ConfigModule } from '$configs/config.module';
 import { EmailModule } from '$email/email.module';
@@ -14,7 +13,7 @@ describe('UsersService', () => {
 	const manager = new TestManager({
 		metadata: {
 			imports: [ConfigModule, PrismaModule, AuthModule, EmailModule],
-			providers: [UsersService, { provide: LuciaFactory, useValue: {} }, RolesService],
+			providers: [UsersService, RolesService],
 		},
 	});
 	let service: UsersService;
