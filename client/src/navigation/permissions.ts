7af04a0091ed45df5591734e33c613c7e5d0039a
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2023-09-04 03:10:19 -0400

    chore: route access check with route id and whitelist
    this change makes it more strict with login : if the page exists and is not considered public by any of the frontend perm checkers, then do not access it.
    
    doing this ensures that any user that isn't connected cannot access any page that isn't either whitelisted or can be navigated to using the on-screen navElements.
    
    using the `route.id` also allows for 404 to pass through. Errors will however be swallowed : let's not make the app allow to error eh :)

--- client/src/navigation/permissions.ts
+++ client/src/navigation/permissions.ts
@@ -6,22 +6,49 @@ import type { RequestEvent } from '@sveltejs/kit';
 import { redirect } from '@sveltejs/kit';
 import { StatusCodes } from 'http-status-codes';
 
+export const urlsWhitelist: string[] = ['/', '/login'];
+
 export function verifyUserAccess(event: RequestEvent) {
 	const {
 		locals: { sessionUser },
 		url,
+		route,
 	} = event;
 
-	if (!userCanAccessNav(sessionUser, navElements, url.pathname)) {
-		throw redirect(StatusCodes.SEE_OTHER, handleLoginRedirect(url));
+	// if no route id, this allow user to see 404 page
+	if (!route.id) {
+		return;
 	}
+
+	if (userCanAccessNav(sessionUser, navElements, url.pathname)) {
+		return;
+	}
+
+	throw redirect(StatusCodes.SEE_OTHER, handleLoginRedirect(url));
 }
 
+/**
+ * Checks if the logged-in user can access the nav element.
+ *
+ * If no nav element exists for the given path and user is logged-out, check for whitelist.
+ * If no nav element exists for the given path but a user is logged-in, allow it.
+ *
+ * If no user but nav element found, check if the nav element is considered public.
+ *
+ * Finally, check roles for both user and nav element.
+ */
 export function userCanAccessNav(user: SessionUser, ...getNavElementArgs: Parameters<typeof getNavElement>) {
 	const userToCheck = user;
 	const currentNav = getNavElement(...getNavElementArgs);
 
 	if (!currentNav) {
+		if (!userToCheck) {
+			// allow logged-out users to access whitelisted urls
+			const [, route] = getNavElementArgs;
+
+			return urlsWhitelist.includes(route);
+		}
+
 		return true;
 	}
 
@@ -29,8 +56,12 @@ export function userCanAccessNav(user: SessionUser, ...getNavElementArgs: Parame
 		return currentNav.isPublic;
 	}
 
-	if (!currentNav.roles?.length || !userToCheck.roles?.length) {
+	if (!currentNav.roles || currentNav.roles.length === 0) {
 		return true;
+	}
+
+	if (!userToCheck.roles?.length) {
+		return false;
 	}
 
 	return rolesIntersect(
