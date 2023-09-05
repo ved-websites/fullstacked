1c1ae5bce44885e0cfac3993f737a1832b3b2b25
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2023-09-05 15:33:33 -0400

    tweak: change task kill to use script PID instead of executable
    this makes it so that if the task is run again, it doesn't kill the running pid of the command, but rather the pid of the script itself, which vscode does not see as an error and doesn't show a error popup
    
    this means that it could kill itself, so a new check was added to the bash condition

--- api/.vscode/scripts/debug-api.sh
+++ api/.vscode/scripts/debug-api.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-API_DEV_PID=$(pgrep -f 'pnpm start:debug' -d ' ')
+API_DEV_PID=$(pgrep -f 'debug-api.sh' -d ' ')
 
 # If there is a api dev server already running, kill it.
-if [ ! -z $API_DEV_PID ]
+if [ ! -z $API_DEV_PID ] && [ $API_DEV_PID != $$ ]
 then
 	kill $API_DEV_PID
 fi
--- client/.vscode/scripts/debug-client.sh
+++ client/.vscode/scripts/debug-client.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-CLIENT_DEV_PID=$(pgrep -f 'vite\.js dev' -d ' ')
+CLIENT_DEV_PID=$(pgrep -f 'debug-client.sh' -d ' ')
 
 # If there is a vite dev server already running, kill it.
-if [ ! -z $CLIENT_DEV_PID ]
+if [ ! -z $CLIENT_DEV_PID ] && [ $CLIENT_DEV_PID != $$ ]
 then
 	kill $CLIENT_DEV_PID
 fi
