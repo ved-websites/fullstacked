6fbfb18b5f123371925fe06e5b1ba6eacfaf7109
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2023-09-25 13:10:17 -0400

    perf: purge UI component library css code too
    this plugins looks to purge css content that is added by the UI component library (in my case Flowbite) so that the CSS used by the components that I do NOT use are also purged.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,11 +4,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { nodeLoaderPlugin } from '@vavite/node-loader/plugin';
 import houdini from 'houdini/vite';
 import { defineConfig, type UserConfig } from 'vite';
+import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 
 export const plugins: UserConfig['plugins'] = [
 	// @ts-ignore Ignoring plugin version incompat
 	houdini(),
 	sveltekit(),
+	purgeCss(),
 	nodeLoaderPlugin(),
 ];
 
