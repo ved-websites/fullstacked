e2a4f55a4c573bb918ccac54a7130067cff029f7
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2023-10-04 23:48:04 -0400

    fix: faulty test that I prob won't ever implement

--- api/src/@common/users/lang.interceptor.spec.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from 'vitest';
-import { LangInterceptor } from './lang.interceptor';
-
-describe('LangInterceptor', () => {
-	it('should be defined', () => {
-		expect(new LangInterceptor()).toBeDefined();
-	});
-});
