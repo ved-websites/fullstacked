2d65f040a512906be692c7e16eb21861b36e8d16
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2023-11-17 21:57:28 -0500

    tests: add [dis]connect code to mocked graphql module
    also, the tsconfig was borked, this fixes the includes field which allows src files to be included in the dev ts, which means access to app types too

--- api/tests/mocks/graphql.module.ts
+++ api/tests/mocks/graphql.module.ts
@@ -2,6 +2,8 @@ import { ConfigModule } from '$configs/config.module';
 import { ContextModule } from '$graphql/context/context.module';
 import { CommonContext, ContextService, TypedSubscriptionContext } from '$graphql/context/context.service';
 import { schemaPath } from '$graphql/graphql.module';
+import { PresenceModule } from '$users/presence/presence.module';
+import { PresenceService } from '$users/presence/presence.service';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GraphQLModule as NestGraphQLModule } from '@nestjs/graphql';
 import depthLimit from 'graphql-depth-limit';
@@ -9,18 +11,37 @@ import { EnvironmentConfig } from '~/env.validation';
 
 export const TestGraphqlModule = NestGraphQLModule.forRootAsync<ApolloDriverConfig>({
 	driver: ApolloDriver,
-	imports: [ConfigModule, ContextModule],
-	useFactory: async (env: EnvironmentConfig, contextService: ContextService) => {
+	imports: [ConfigModule, ContextModule, PresenceModule],
+	useFactory: async (env: EnvironmentConfig, contextService: ContextService, presenceService: PresenceService) => {
 		return {
 			typePaths: [schemaPath],
 			installSubscriptionHandlers: true,
 			subscriptions: {
-				'graphql-ws': true,
+				'graphql-ws': {
+					onConnect: async (context) => {
+						const {
+							req: { session },
+						} = await contextService.setupGqlContext(context as TypedSubscriptionContext | CommonContext);
+
+						if (session) {
+							presenceService.onConnect(session);
+						}
+					},
+					onDisconnect: async (context) => {
+						const {
+							req: { session },
+						} = contextService.extractRawGqlContext(context as TypedSubscriptionContext | CommonContext);
+
+						if (session) {
+							presenceService.onDisconnect(session);
+						}
+					},
+				},
 			},
 			playground: false,
 			validationRules: [depthLimit(env.GRAPHQL_DEPTH_LIMIT)],
 			context: (context: TypedSubscriptionContext | CommonContext) => contextService.setupGqlContext(context),
 		};
 	},
-	inject: [EnvironmentConfig, ContextService],
+	inject: [EnvironmentConfig, ContextService, PresenceService],
 });
