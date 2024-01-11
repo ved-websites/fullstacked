4aa7351302f7f371e24aea3ee478f1c8a77aea48
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2024-01-11 02:29:21 -0500

    fix: missing setupApp in ensureGraphqlSchema

--- api/src/@common/graphql/schema/schema.manager.ts
+++ api/src/@common/graphql/schema/schema.manager.ts
@@ -1,12 +1,18 @@
+import { setupApp } from '$app/setupApp';
 import { schemaPath } from '$graphql/graphql.module';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { existsSync } from 'fs';
 import { AppModule } from '~app-module';
 
+/**
+ * If schema file doesn't exist, run app once to generate schema
+ */
 export async function ensureGraphQLSchema() {
 	if (!existsSync(schemaPath)) {
-		// If schema file doesn't exist, run app once to generate schema
-		const tempGqlApp = await NestFactory.create(AppModule);
+		const tempGqlApp = await NestFactory.create<NestExpressApplication>(AppModule);
+
+		setupApp(tempGqlApp);
 
 		await tempGqlApp.init();
 		await tempGqlApp.close();
