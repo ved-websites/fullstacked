8eae9570728573cf9908305b586c307a2fa31791
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2024-02-09 13:37:45 -0500

    fix: delete auth cookie on session unauthorized
    this fixes a redirect loop on db generate (where sessions are not valid)
    
    this in theory should also fix a case where invalid sessions that exist throw an error
    
    basically, if the session expires and the refresh mechanism is not called within the time limit, the server throws an `UnauthorizedException`, so the client's auth-handler fails, but instead of redirecting to calling it again with an expired cookie, it deletes the cookie before redirecting, preventing a bad cookie from making the redirect loop happen

--- client/src/auth/auth-handler.ts
+++ client/src/auth/auth-handler.ts
@@ -1,8 +1,16 @@
 import type { TsRestClient } from '$lib/ts-rest/client';
 import { assertTsRestResultOK } from '$lib/utils/assertions';
+import { StatusCodes } from 'http-status-codes';
+import { SESSION_COOKIE_NAME } from '~shared';
 
 export async function getAuthUser(tsrest: TsRestClient) {
-	const result = await tsrest.auth.session();
+	const result = await tsrest.auth.session({
+		onCommonError: ({ data, event }) => {
+			if (event && data.status === StatusCodes.UNAUTHORIZED) {
+				event.cookies.delete(SESSION_COOKIE_NAME, { path: '/' });
+			}
+		},
+	});
 
 	assertTsRestResultOK(result);
 
