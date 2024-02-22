c1a92ec7fef58329e34e1a5eb854a9796ba57741
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2024-02-22 00:42:08 -0500

    perf: use blob instead of multiple json conversions

--- client/src/routes/api/[...paths]/+server.ts
+++ client/src/routes/api/[...paths]/+server.ts
@@ -4,12 +4,12 @@ import type { RequestHandler } from './$types';
 export const POST: RequestHandler = async ({ fetch, request, params }) => {
 	const apiUrl = getApiUrl(`/api/${params.paths}`);
 
-	const body = await request.json();
+	const body = await request.blob();
 
 	const res = await fetch(apiUrl.href, {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
-		body: JSON.stringify(body),
+		body,
 	});
 
 	return res;
