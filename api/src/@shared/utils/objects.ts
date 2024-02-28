593774823d1f650e3ba56a2f6221a60de51eb929
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2024-02-28 13:06:46 -0500

    chore: add `Prettify` to `removeKeys` output type
    this makes the new type much cleaner, rather than seeing `Omit<{dump}, 'keys' | 'removed'>`

--- api/src/@shared/utils/objects.ts
+++ api/src/@shared/utils/objects.ts
@@ -1,4 +1,4 @@
-export function removeKeys<T extends Record<string, unknown>, K extends keyof T>(obj: T, ...keysToRemove: K[]): Omit<T, K> {
+export function removeKeys<T extends Record<string, unknown>, K extends keyof T>(obj: T, ...keysToRemove: K[]): Prettify<Omit<T, K>> {
 	const newObj = { ...obj };
 
 	keysToRemove.forEach((key) => delete newObj[key]);
