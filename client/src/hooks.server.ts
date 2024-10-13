6e087f202e6122a5fdc9397d84fff730ba98bf8f
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2024-10-13 05:31:43 -0400

    Revert "fix: set cookies to api domain"
    This reverts commit a3a245ea5d007c0a9bddf76a6093151810b44714.

--- client/src/hooks.server.ts
+++ client/src/hooks.server.ts
@@ -1,8 +1,7 @@
 import { dev } from '$app/environment';
-import { PUBLIC_SENTRY_DSN } from '$env/static/public';
+import { PUBLIC_API_ADDR, PUBLIC_SENTRY_DSN } from '$env/static/public';
 import { getBrowserLang } from '$i18n';
 import { createTsRestClient } from '$lib/ts-rest/client';
-import { getApiUrl } from '$lib/utils';
 import * as Sentry from '@sentry/sveltekit';
 import type { Handle, HandleFetch } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
@@ -61,6 +60,18 @@ export const handle: Handle = sequence(Sentry.sentryHandle(), async ({ event, re
 });
 
 export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
+	if (request.url.startsWith(PUBLIC_API_ADDR)) {
+		const passedHeaders: string[] = ['cookie', 'accept-language'];
+
+		passedHeaders.forEach((header) => {
+			const headerValue = event.request.headers.get(header);
+
+			if (headerValue) {
+				request.headers.set(header, headerValue);
+			}
+		});
+	}
+
 	const response = await fetch(request).catch(() => {
 		const fakeResponse = { message: 'common.errors.server.down' satisfies I18nKey };
 
@@ -79,22 +90,12 @@ export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
 
 	const cookies: string[] = Array.isArray(headerCookies) ? headerCookies : [headerCookies];
 
-	const apiUrl = getApiUrl();
-
 	cookies.forEach((cookie) => {
 		const { name, value, ...opts } = parseString(cookie);
 
 		event.cookies.set(name, value, {
 			path: '/',
 			...opts,
-			// @ts-expect-error set-cookie-parser has string instead of strict type
-			sameSite: opts.sameSite,
-		});
-
-		event.cookies.set(name, value, {
-			path: '/',
-			...opts,
-			domain: apiUrl.hostname,
 			// @ts-expect-error set-cookie-parser has string instead of strict type
 			sameSite: opts.sameSite,
 		});
